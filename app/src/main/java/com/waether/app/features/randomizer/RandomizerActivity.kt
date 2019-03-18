56ffc9b5e8cbe10636a34af48fa7ff6e076b8ed0
gomanjy <lion2010man@outlook.com>
2019-03-18 22:03:01 +0200

    simple live data update

---
+++ app/src/main/java/com/waether/app/features/randomizer/RandomizerActivity.kt
@@ -0,0 +1,20 @@
+package com.waether.app.features.randomizer
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.waether.app.R
+import kotlinx.android.synthetic.main.activity_randomizer.*
+
+class RandomizerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setContentView(R.layout.activity_randomizer)
+        super.onCreate(savedInstanceState)
+
+        val viewModel = ViewModelProviders.of(this).get(RandomizerViewModel::class.java)
+        viewModel.randomNumber.observe(this,
+            Observer { random_number_textView.text = it.toString() }
+            )
+    }
+}
