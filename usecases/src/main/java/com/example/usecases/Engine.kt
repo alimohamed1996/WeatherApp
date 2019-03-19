9ff0b8fa5484b517a3b36fa32d2da40cdd7aec2c
gomanjy <lion2010man@outlook.com>
2019-03-19 21:43:17 +0200

    add entities for ROOM

--- usecases/src/main/java/com/example/usecases/Engine.kt
+++ usecases/src/main/java/com/example/usecases/Engine.kt
@@ -1,6 +1,12 @@
 package com.example.usecases
 
 import android.os.CountDownTimer
+import android.util.Log
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 private const val FINISH_AFTER_MILLIS = 10 * 1000 * 60L
 private const val INTERVAL_IN_MILLIS = 1 * 1000L
@@ -12,5 +18,16 @@ class Ticker (private val onTicking: (Long) -> Unit)
 
     override fun onTick(millisUntilFinished: Long) {
         onTicking(millisUntilFinished)
+    }
+}
+
+fun tickerObserval( onTicking: (Long) -> Unit)  {
+    Observable.intervalRange(0L,10L,0L , 1L , TimeUnit.SECONDS)
+    //Observable.interval(2,TimeUnit.MILLISECONDS)
+        .subscribeOn(AndroidSchedulers.mainThread())
+        .observeOn(Schedulers.io())
+        .subscribe {
+        Log.d("Interval" , it.toString())
+        onTicking(it)
     }
 }
--- usecases/src/main/java/com/example/usecases/usecases.kt
+++ usecases/src/main/java/com/example/usecases/usecases.kt
@@ -1,3 +1,11 @@
 package com.example.usecases
 
+import android.arch.lifecycle.MutableLiveData
+
 fun randomNumberGenerator() = Math.random() * 1000
+
+fun incrementNumber(incrementValue: Int = 1 , mutableLiveData: MutableLiveData<Int>){
+    val oldValue = mutableLiveData.value ?: 0
+    mutableLiveData.postValue(oldValue + incrementValue)
+
+}
